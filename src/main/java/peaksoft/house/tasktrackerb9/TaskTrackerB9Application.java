2b285047946a6d1bcd7c38a8fa28b2777477e0d0
Datka Mamatzhanova <datkascnu@gmail.com>
2023-07-03 16:40:25 +0600

    first  commit

---
+++ src/main/java/peaksoft/house/tasktrackerb9/TaskTrackerB9Application.java
@@ -0,0 +1,13 @@
+package peaksoft.house.tasktrackerb9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerB9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerB9Application.class, args);
+	}
+
+}
---
+++ src/test/java/peaksoft/house/tasktrackerb9/TaskTrackerB9ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.house.tasktrackerb9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerB9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
