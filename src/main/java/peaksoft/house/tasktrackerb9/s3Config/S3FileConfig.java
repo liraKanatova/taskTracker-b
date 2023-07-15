9e81c7adbb21007ceb32da765fd202269e7371a3
LiraKanatova <lira.kanaatova.ch@gmail.com>
2023-07-15 16:09:07 +0600

    Commit fixed

--- src/main/java/peaksoft/house/tasktrackerb9/s3Config/S3FileConfig.java
+++ src/main/java/peaksoft/house/tasktrackerb9/s3Config/S3FileConfig.java
@@ -1,4 +1,5 @@
 package peaksoft.house.tasktrackerb9.s3Config;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +7,6 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-
 
 @Configuration
 public class S3FileConfig {
@@ -16,7 +16,6 @@ public class S3FileConfig {
     private String AWS_SECRET_KEY;
     @Value("${aws_region}")
     private String REGION;
-
 
     @Bean
     S3Client s3Client() {
--- src/main/java/peaksoft/house/tasktrackerb9/s3Config/S3Service.java
+++ src/main/java/peaksoft/house/tasktrackerb9/s3Config/S3Service.java
@@ -1,4 +1,5 @@
 package peaksoft.house.tasktrackerb9.s3Config;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -7,13 +8,13 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+
 import java.io.IOException;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
-
     @Value("${aws_bucket_name}")
     private String bucketName;
     @Value("${aws_bucket_path}")
