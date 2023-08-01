a79711de3996e3866c0aa5aa0e6f7508adf621aa
Malik <asanbekovmalik2@gmail.com>
2023-08-01 15:20:13 +0600

    fix method global-search

--- src/main/java/peaksoft/house/tasktrackerb9/dto/response/CardResponse.java
+++ src/main/java/peaksoft/house/tasktrackerb9/dto/response/CardResponse.java
@@ -1,18 +1,24 @@
 package peaksoft.house.tasktrackerb9.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class CardResponse {
 
     private Long cardId;
     private String title;
+    private String duration;
+    private int numberUsers;
+    private int numberItems;
+    private int numberCompletedItems;
+    private List<LabelResponse> labelResponses;
+    private List<CommentResponse> commentResponses;
 
     public CardResponse(Long cardId, String title) {
         this.cardId = cardId;
---
+++ src/main/java/peaksoft/house/tasktrackerb9/dto/response/CommentResponse.java
@@ -0,0 +1,18 @@
+package peaksoft.house.tasktrackerb9.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class CommentResponse {
+
+    private Long commentId;
+    private String comment;
+    private String createdDate;
+
+
+
+}
