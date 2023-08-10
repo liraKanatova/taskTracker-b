49399cedc887e069393f2466107ffd3f794e03eb
LiraKanatova <lira.kanaatova.ch@gmail.com>
2023-08-10 13:25:13 +0600

    fixed

--- src/main/java/peaksoft/house/tasktrackerb9/api/S3Api.java
+++ src/main/java/peaksoft/house/tasktrackerb9/api/S3Api.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import peaksoft.house.tasktrackerb9.services.impl.S3Service;
