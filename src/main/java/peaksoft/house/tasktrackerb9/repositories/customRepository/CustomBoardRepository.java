faa157928300d3181eba671e46ced7eeecf7c0eb
Ruslan <rusi.studio.kgz@gmail.com>
2023-08-29 18:11:25 +0600

    add method gerAllArchives

--- src/main/java/peaksoft/house/tasktrackerb9/repositories/customRepository/CustomBoardRepository.java
+++ src/main/java/peaksoft/house/tasktrackerb9/repositories/customRepository/CustomBoardRepository.java
@@ -1,10 +1,14 @@
 package peaksoft.house.tasktrackerb9.repositories.customRepository;
 
 import peaksoft.house.tasktrackerb9.dto.response.BoardResponse;
+import peaksoft.house.tasktrackerb9.dto.response.GetAllArchiveResponse;
 
 import java.util.List;
 
 public interface CustomBoardRepository {
 
     List<BoardResponse> getAllBoardsByWorkspaceId(Long workSpaceId);
+
+    GetAllArchiveResponse getAllArchivedCardsAndColumns(Long boardId);
+
 }
