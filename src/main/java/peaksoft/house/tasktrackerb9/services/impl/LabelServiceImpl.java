a64db2a8498d424b37ac463858425676c0f0adcc
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-09-21 19:14:04 +0600

    refactor init label card

--- src/main/java/peaksoft/house/tasktrackerb9/services/impl/LabelServiceImpl.java
+++ src/main/java/peaksoft/house/tasktrackerb9/services/impl/LabelServiceImpl.java
@@ -9,6 +9,7 @@ import peaksoft.house.tasktrackerb9.dto.request.LabelRequest;
 import peaksoft.house.tasktrackerb9.dto.response.LabelResponse;
 import peaksoft.house.tasktrackerb9.dto.response.SimpleResponse;
 import peaksoft.house.tasktrackerb9.exceptions.AlreadyExistException;
+import peaksoft.house.tasktrackerb9.exceptions.BadCredentialException;
 import peaksoft.house.tasktrackerb9.exceptions.NotFoundException;
 import peaksoft.house.tasktrackerb9.models.Card;
 import peaksoft.house.tasktrackerb9.models.Label;
@@ -19,7 +20,9 @@ import peaksoft.house.tasktrackerb9.services.LabelService;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -56,14 +59,23 @@ public class LabelServiceImpl implements LabelService {
             log.error(String.format("Label with id : %s doesn't exist!", labelId));
             return new NotFoundException(String.format("Label with id : %s doesn't exist!", labelId));
         });
+
+        List<String> allowedColors = Arrays.asList("Green", "Blue", "Red", "Yellow");
+
+        if (!allowedColors.contains(label.getColor())) {
+            throw new BadCredentialException(String.format("Label color '%s' is not allowed.", label.getColor()));
+        }
+
         for (Label l : card.getLabels()) {
             if (l.getLabelName().equalsIgnoreCase(label.getLabelName()) && l.getColor().equalsIgnoreCase(label.getColor())) {
                 throw new AlreadyExistException(String.format("Label with name '%s' and color '%s' already exists on the card!", label.getLabelName(), label.getColor()));
             }
         }
+
         if (card.getLabels().stream().anyMatch(l -> l.getId().equals(labelId))) {
             throw new AlreadyExistException(String.format("Label with id '%s' already exists on the card!", labelId));
         }
+
         label.getCards().add(card);
         card.getLabels().add(label);
         cardRepository.save(card);
@@ -75,6 +87,7 @@ public class LabelServiceImpl implements LabelService {
                 .message(String.format("Label with name: %s successfully added to card with name: %s", label.getLabelName(), card.getTitle()))
                 .build();
     }
+
 
     @Override
     public LabelResponse getLabelById(Long labelId) {
@@ -98,18 +111,23 @@ public class LabelServiceImpl implements LabelService {
     }
 
     @Override
-    public SimpleResponse deleteLabelById(Long labelId) {
-        Label label = labelRepository.findById(labelId).orElseThrow(() -> {
-            log.error(String.format("Label with id: %s doesn't exist !", labelId));
-            return new NotFoundException(String.format("Label with id: %s doesn't exist !", labelId));
+    public SimpleResponse deleteFromCardByIdLables(Long cardId, Long labelId) {
+        Card card = cardRepository.findById(cardId).orElseThrow(()->{
+            log.error("Card with id: " + cardId + "not found");
+            return new NotFoundException("Card with id: " + cardId + "not found");
         });
-        labelRepository.delete(label);
-        log.info(String.format("Label with name : %s  successfully deleted!", label.getLabelName()));
-        return SimpleResponse.builder()
-                .status(HttpStatus.OK)
-                .message(String.format("Label with name : %s  successfully deleted!", label.getLabelName()))
-                .build();
+        Label label = labelRepository.findById(labelId).orElseThrow(()->{
+            log.error("Label with id: " + cardId + "not found");
+            return new NotFoundException("Label with id: " + cardId + "not found");
+        });
+        card.getLabels().remove(label);
+        label.getCards().remove(card);
+        cardRepository.save(card);
+        labelRepository.save(label);
+
+        return new SimpleResponse("Метка успешно удалена из карты.", HttpStatus.OK);
     }
+
 
     @Override
     public List<LabelResponse> getAllLabelsByCardId(Long cardId) {
